cb993a0b14d404e0e105e27a94daba2718389e7b
flonix8 <flstanek@googlemail.com>
2023-09-13 10:50:11 +0200

    Implement consumer and publisher as context managers

--- python/visionlib/pipeline/consumer.py
+++ python/visionlib/pipeline/consumer.py
@@ -1,39 +1,57 @@
-import redis
-from typing import List
+import logging
 from collections import defaultdict
+from typing import List
+
+import pybase64
+import redis
+
+logger = logging.getLogger(__name__)
 
 class RedisConsumer:
-    def __init__(self, host: str, port: int, stream_keys: List[str]) -> None:
-        self._redis_client = redis.Redis(host, port)
+    def __init__(self, host: str, port: int, stream_keys: List[str], b64_decode=True) -> None:
+        self._redis_client = None
+        self._host = host
+        self._port = port
         self._stream_keys = stream_keys
+        self._b64_decode = b64_decode
 
         self._last_retrieved_ids = defaultdict(lambda: '$')
+
+    def __enter__(self):
+        self._redis_client = redis.Redis(self._host, self._port)
+        return self
+
+    def __call__(self) -> bytes:
+        data_field_name = b'proto_data_b64' if self._b64_decode else b'proto_data'
         
-        self._generating = False
-
-    def __call__(self):
-        self._generating = True
-
-        while self._generating:
-            result = self._redis_client.xread(
-                count=1,
-                block=2000,
-                streams={key: self._last_retrieved_ids[key] 
-                            for key in self._stream_keys}
-            )
-            
-            if result is None:
-                yield None
-            
-            for item in result:
-                proto_b64 = item[1][0][1][b'proto_data_b64']
-                stream_key = item[0].decode('utf-8')
-
-                self._last_retrieved_ids[stream_key] = item[1][0][0].decode('utf-8')
-
-                yield proto_b64
+        result = self._redis_client.xread(
+            count=1,
+            block=2000,
+            streams={key: self._last_retrieved_ids[key] 
+                        for key in self._stream_keys}
+        )
         
-        self._redis_client.close()
-    
-    def stop(self):
-        self._generating = False
+        if result is None:
+            yield None
+        
+        for item in result:
+            proto_data = item[1][0][1][data_field_name]
+            stream_key = item[0].decode('utf-8')
+
+            self._last_retrieved_ids[stream_key] = item[1][0][0].decode('utf-8')
+
+            if self._b64_decode:
+                yield pybase64.b64decode(proto_data, validate=True)
+            else:
+                yield proto_data
+        
+    def __exit__(self, _, exc_value, __):
+        if exc_value is not None:
+            logger.error('Redis read yielded exception', exc_info=exc_value)
+
+        try:
+            self._redis_client.close()
+        except Exception as e:
+            logger.warn('Error while closing redis client', exc_info=e)
+        
+        return True
--- python/visionlib/pipeline/publisher.py
+++ python/visionlib/pipeline/publisher.py
@@ -1,15 +1,43 @@
+import logging
+
+import pybase64
 import redis
 
+logger = logging.getLogger(__name__)
+
 class RedisPublisher:
-    def __init__(self, host: str, port: int, stream_maxlen=10) -> None:
-        self._redis_client = redis.Redis(host, port)
+    def __init__(self, host: str, port: int, stream_maxlen=10, b64_encode=True) -> None:
+        self._redis_client = None
         self._host = host
         self._port = port
         self._stream_maxlen = 10
+        self._b64_encode = b64_encode
 
-    def __call__(self, stream_key: str, proto_data_b64: str):
+    def __enter__(self):
+        self._redis_client = redis.Redis(self._host, self._port)
+        return self
+
+    def __call__(self, stream_key: str, proto_data: bytes):
+        data_field_name = 'proto_data'
+        data_field_value = proto_data
+
+        if self._b64_encode:
+            data_field_name = 'proto_data_b64'
+            data_field_value = pybase64.b64encode(proto_data)
+
         self._redis_client.xadd(
             name=stream_key, 
-            fields={'proto_data_b64': proto_data_b64}, 
+            fields={data_field_name: data_field_value}, 
             maxlen=self._stream_maxlen
         )
+    
+    def __exit__(self, _, exc_value, __):
+        if exc_value is not None:
+            logger.error('Redis read yielded exception', exc_info=exc_value)
+
+        try:
+            self._redis_client.close()
+        except Exception as e:
+            logger.warn('Error while closing redis client', exc_info=e)
+        
+        return True
