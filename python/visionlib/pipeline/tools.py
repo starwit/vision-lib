93f56d04ca864da79c5eca1f786eee4e7464e347
flonix8 <flstanek@googlemail.com>
2023-10-18 11:55:47 +0200

    Fix field presence detection

--- python/visionlib/pipeline/tools.py
+++ python/visionlib/pipeline/tools.py
@@ -4,12 +4,9 @@ from visionapi.messages_pb2 import VideoFrame
 jpeg = TurboJPEG()
 
 def get_raw_frame_data(proto: VideoFrame):
-    try:
-        jpeg_field_defined = proto.HasField('frame_data_jpeg')
-        jpeg_field_set = len(proto.frame_data_jpeg) > 0
-        if jpeg_field_defined and jpeg_field_set:
-            return jpeg.decode(proto.frame_data_jpeg)
-    except ValueError:
-        pass
-    
-    return proto.frame_data
+    # ListFields() returns all defined fields that are set (for primitive values: do not have their default value)
+    jpeg_field_set = 'frame_data_jpeg' in map(lambda f: f[0].name, proto.ListFields())
+    if jpeg_field_set:
+        return jpeg.decode(proto.frame_data_jpeg)
+    else:
+        return proto.frame_data
