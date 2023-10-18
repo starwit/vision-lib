e46acc59d80c098dc6b07d1b75c239b772bb1ed6
flonix8 <flstanek@googlemail.com>
2023-10-18 11:38:15 +0200

    Fix proto field detection

--- python/visionlib/pipeline/tools.py
+++ python/visionlib/pipeline/tools.py
@@ -4,7 +4,12 @@ from visionapi.messages_pb2 import VideoFrame
 jpeg = TurboJPEG()
 
 def get_raw_frame_data(proto: VideoFrame):
-    if proto.HasField('frame_data_jpeg'):
-        return jpeg.decode(proto.frame_data_jpeg)
-    else:
-        return proto.frame_data
+    try:
+        jpeg_field_defined = proto.HasField('frame_data_jpeg')
+        jpeg_field_set = len(proto.frame_data_jpeg) > 0
+        if jpeg_field_defined and jpeg_field_set:
+            return jpeg.decode(proto.frame_data_jpeg)
+    except ValueError:
+        pass
+    
+    return proto.frame_data
