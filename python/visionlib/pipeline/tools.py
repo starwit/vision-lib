ca991c4955ddb57e908a025dbef7c67cac56793e
flonix8 <flstanek@googlemail.com>
2024-02-23 16:25:22 +0100

    Version 0.7.0: Remove vision-api dependency; Add some proto validation

--- python/visionlib/pipeline/tools.py
+++ python/visionlib/pipeline/tools.py
@@ -1,13 +1,35 @@
 import numpy as np
 from turbojpeg import TurboJPEG
-from visionapi.messages_pb2 import VideoFrame
 
 jpeg = TurboJPEG()
 
-def get_raw_frame_data(proto: VideoFrame):
-    # ListFields() returns all defined fields that are set (for primitive values: do not have their default value)
-    jpeg_field_set = 'frame_data_jpeg' in map(lambda f: f[0].name, proto.ListFields())
-    if jpeg_field_set:
+def is_valid_video_frame(proto):
+    try:
+        # ListFields() returns all defined fields that are set (for primitive values: do not have their default value)
+        set_fields = list(map(lambda f: f[0].name, proto.ListFields()))
+        if 'shape' in set_fields and any(('frame_data_jpeg' in set_fields, 'frame_data' in set_fields)):
+            return True
+        else:
+            return False
+    except:
+        return False
+    
+def is_jpeg_frame(proto):
+    set_fields = list(map(lambda f: f[0].name, proto.ListFields()))
+    if 'frame_data_jpeg' in set_fields:
+        return True
+    else:
+        return False
+
+def get_raw_frame_data(proto):
+    '''
+        Gets image data as NDArray from a VideoFrame proto. JPEG frames are handled (i.e. decompressed) transparently.\ 
+        If necessary fields are missing returns None.
+    '''
+    if not is_valid_video_frame(proto):
+        return None
+    
+    if is_jpeg_frame(proto):
         return jpeg.decode(proto.frame_data_jpeg)
     else:
         image_dims = (proto.shape.height, proto.shape.width, proto.shape.channels)
