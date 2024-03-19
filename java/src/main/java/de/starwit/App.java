9a1a6c9e09b1731d3bcd516c1c231a84a4bb33c5
flonix8 <flstanek@googlemail.com>
2024-03-19 17:49:06 +0100

    Add more implementation to java-based `SaeReader` (somewhat working)

--- java/src/main/java/de/starwit/App.java
+++ java/src/main/java/de/starwit/App.java
@@ -1,5 +1,7 @@
 package de.starwit;
 
 public class App {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- java/src/main/java/de/starwit/pipeline/SaeReader.java
+++ java/src/main/java/de/starwit/pipeline/SaeReader.java
@@ -1,29 +1,94 @@
 package de.starwit.pipeline;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
 import de.starwit.visionapi.Messages.SaeMessage;
 import redis.clients.jedis.JedisPooled;
 import redis.clients.jedis.StreamEntryID;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 import redis.clients.jedis.params.XReadParams;
+import redis.clients.jedis.resps.StreamEntry;
 
-public class SaeReader {
+public class SaeReader implements Closeable {
 
-    private XReadParams xReadParams;
+    private static final Logger log = LoggerFactory.getLogger(SaeReader.class);
+
     private Map<String,StreamEntryID> streamPointerById;
-    private JedisPooled jedisClient;
+    private JedisPooled redisClient;
 
     public SaeReader(List<String> streamIds, String host, int port) {
-        this.jedisClient = new JedisPooled(host, port);
-        this.xReadParams = new XReadParams().count(10).block(200);
+        this.redisClient = new JedisPooled(host, port);
         this.streamPointerById = streamIds.stream()
                 .collect(Collectors.toMap(id -> id, id -> StreamEntryID.LAST_ENTRY));
     }
-    
-    public Optional<SaeMessage> getNext() {
-        return Optional.empty();
+
+    public List<SaeMessage> read(int maxCountPerStream, int timeout) {
+        XReadParams xReadParams = new XReadParams().count(maxCountPerStream).block(timeout);
+        List<Map.Entry<String, List<StreamEntry>>> result = null;
+        try {
+            result = this.redisClient.xread(xReadParams, this.streamPointerById);
+        } catch (JedisConnectionException ex) {
+            log.warn("Could not read from redis", ex);
+            return new ArrayList<>();
+        }
+
+        if (result == null) {
+            return new ArrayList<>();
+        }
+
+        List<SaeMessage> parsedResults = new ArrayList<>();
+
+        for (Map.Entry<String,List<StreamEntry>> resultEntry : result) {
+            String streamId = resultEntry.getKey();
+            List<StreamEntry> messages = resultEntry.getValue();;
+            for (StreamEntry message : messages) {
+                // Set last retrieved id
+                updateStreamPointers(streamId, message);
+                String proto_b64 = message.getFields().get("proto_data_b64");
+                try {
+                    SaeMessage msg = SaeMessage.parseFrom(Base64.getDecoder().decode(proto_b64));
+                    parsedResults.add(msg);
+                } catch (InvalidProtocolBufferException ex) {
+                    log.error("Error decoding proto from message. streamId=" + streamId, ex);
+                }
+            }
+        }
+
+        return parsedResults;
+    }
+
+    private void updateStreamPointers(String streamId, StreamEntry msg) {
+        this.streamPointerById.put(streamId, msg.getID());
+
+        // Set any remaining '$' stream pointers (i.e. LAST_ENTRY) to a concrete timestamp
+        // If we do not do this, some streams are not picked up when listening to multiple streams
+        List<String> lastEntryStreamIds = this.streamPointerById.entrySet().stream()
+                .filter(e -> e.getValue().equals(StreamEntryID.LAST_ENTRY))
+                .map(Map.Entry::getKey)
+                .toList();
+            
+        lastEntryStreamIds.stream()
+                .forEach(id -> this.streamPointerById.put(id, msg.getID()));
+    }
+
+    @Override
+    public void close() {
+        try {
+            this.redisClient.close();
+        } catch (Exception ex) {
+            log.warn("Error while closing", ex);
+        }
     }
 }
