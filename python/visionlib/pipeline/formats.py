fee8b79b543615e9d9d71cbeff60d125702b2229
flonix8 <flstanek@googlemail.com>
2025-07-25 12:46:47 +0200

    Version 0.13.0: Add `is_position_message()`

--- python/visionlib/pipeline/formats.py
+++ python/visionlib/pipeline/formats.py
@@ -8,3 +8,11 @@ def is_sae_message(msg: 'SaeMessage') -> bool:
         msg.frame.HasField('shape'),
         msg.HasField('metrics')
     ))
+
+def is_position_message(msg: 'PositionMessage') -> bool:
+    '''This function takes an 'PositionMessage' object and tries to determine if it is a valid 
+       'PositionMessage' by checking it for mandatory fields.'''
+    return all((
+        msg.HasField('geo_coordinate'),
+        msg.timestamp_utc_ms != 0,
+    ))
